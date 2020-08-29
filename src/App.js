c8879452179e30dd31b12f6327c2b7bbab5f5a6a
Nate Davis <orpheusdevelopment@gmail.com>
2020-08-29 10:55:10 -0500

    Creates navbar component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 
 function App() {
   return (
+    <Navbar></Navbar>
+  )
+}
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav"></ul>
+    </nav>
   );
 }
 
