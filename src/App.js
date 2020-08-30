e32562f8e0535263652cb0f2dfd96404d158cc39
Nate Davis <orpheusdevelopment@gmail.com>
2020-08-30 13:59:54 -0500

    Creates DropdownItem + DropdownMenu Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as BellIcon} from './icons/bell.svg';
 import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
 import { ReactComponent as CaretIcon } from './icons/caret.svg';
@@ -17,8 +17,41 @@ function App() {
       <NavItem icon={<PlusIcon />} />
       <NavItem icon={<BellIcon />} />
       <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CaretIcon />}>
+
+        {/* {Dropdown goes here} */}
+        <DropdownMenu></DropdownMenu>
+
+      </NavItem>
     </Navbar>
   )
+}
+
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+
+        {props.children}
+
+        <span className="icon-right">{props.leftRight}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem 
+        leftIcon={<CogIcon /> }
+        rightIcon={<ChevronIcon /> }>
+        Settings
+      </DropdownItem>
+    </div>
+  );
 }
 
 function Navbar(props) {
@@ -30,11 +63,16 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+
+      {open && props.children}
     </li>
   );
 }
